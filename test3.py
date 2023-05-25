ccf6647f76ad1049284421e9b4e018e5e1c9f2f8
dcmotocorp <dipenpatell2496.com>
2023-05-25 10:12:18 +0530

    yes

---
+++ test3.py
@@ -0,0 +1,29 @@
+import pygit2
+import os 
+# Set the path to the GitLab repository
+repo_path = '/path/to/repository'
+
+# Open the repository
+repo = pygit2.Repository(os.path.dirname(os.path.abspath(__file__)))
+
+# Get the master branch
+master_branch = repo.branches['master']
+
+# Iterate over all branches
+for branch_name in repo.branches.remote:
+    # Skip the master branch
+    if branch_name == 'master':
+        continue
+
+    # Get the branch reference
+    branch_ref = repo.branches[branch_name]
+
+    # Rebase the branch onto the latest changes in master
+    repo.checkout(branch_ref)
+    rebase = repo.rebase(branch_ref.target, onto=master_branch.target)
+    rebase.finish(repo.default_signature, strategy=pygit2.GIT_REBASE_OPERATION.NONE)
+
+    # Push the rebased branch to the GitLab repository
+    remote_name = 'origin'
+    remote = repo.remotes[remote_name]
+    remote.push([f'{branch_name}:{branch_name}'])
